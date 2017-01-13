96c18d58183891a090ea0d4564de099d2fb83b6d
Miha <Miha@MIHA>
2017-01-13 20:22:50 +0200

    Add project files.

---
+++ Keybo_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Keybo_2.MvcApplication" Language="C#" %>
---
+++ Keybo_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Keybo_2.Startup))]
+namespace Keybo_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
